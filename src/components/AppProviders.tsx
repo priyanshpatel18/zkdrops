b7cc83386b5e20c4d0b066d1c570294d57dca9f2
priyanshpatel18 <priyansh.180804@gmail.com>
2025-05-14 03:40:51 +0530

    added mobile adapters

--- src/components/AppProviders.tsx
+++ src/components/AppProviders.tsx
@@ -1,25 +1,16 @@
 'use client'
 
-import ThemeProvider from '@/components/ThemeProvider'
-import { ReactQueryProvider } from './ReactQueryProvider'
 import { SolanaProvider } from '@/components/solana/SolanaProvider'
-import React, { useMemo } from 'react'
-import { WalletProvider } from '@solana/wallet-adapter-react'
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import ThemeProvider from '@/components/ThemeProvider'
+import React from 'react'
+import { ReactQueryProvider } from './ReactQueryProvider'
 
 export function AppProviders({ children }: Readonly<{ children: React.ReactNode }>) {
-  const wallets = useMemo(() => {
-    return [new PhantomWalletAdapter()]
-  }, [])
-
   return (
     <ReactQueryProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         <SolanaProvider>
-          <WalletProvider wallets={wallets} autoConnect>
-            <WalletModalProvider>{children}</WalletModalProvider>
-          </WalletProvider>
+          {children}
         </SolanaProvider>
       </ThemeProvider>
     </ReactQueryProvider>
--- src/components/solana/SolanaProvider.tsx
+++ src/components/solana/SolanaProvider.tsx
@@ -1,21 +1,35 @@
 'use client'
 
-import { createSolanaDevnet, createSolanaLocalnet, createWalletUiConfig, WalletUi } from '@wallet-ui/react'
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom"
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare"
+import { CoinbaseWalletAdapter } from "@solana/wallet-adapter-coinbase"
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
+import { clusterApiUrl } from '@solana/web3.js'
 import '@wallet-ui/tailwind/index.css'
 import dynamic from 'next/dynamic'
-import { ReactNode } from 'react'
+import { ReactNode, useMemo } from 'react'
 
 export const WalletButton = dynamic(async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton, {
   ssr: false,
 })
-export const ClusterButton = dynamic(async () => (await import('@wallet-ui/react')).WalletUiClusterDropdown, {
-  ssr: false,
-})
-
-const config = createWalletUiConfig({
-  clusters: [createSolanaDevnet(), createSolanaLocalnet()],
-})
 
 export function SolanaProvider({ children }: { children: ReactNode }) {
-  return <WalletUi config={config}>{children}</WalletUi>
+  // const network =
+  //   process.env.NEXT_PUBLIC_NODE_ENV === "development"
+  //     ? WalletAdapterNetwork.Devnet
+  //     : WalletAdapterNetwork.Mainnet;
+  const endpoint = useMemo(() => clusterApiUrl("devnet"), []);
+
+  const wallets = useMemo(() => {
+    return [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new CoinbaseWalletAdapter()];
+  }, []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
 }
